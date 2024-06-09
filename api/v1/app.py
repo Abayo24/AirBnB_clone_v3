897744ff5b6c6637fc56e1f88845232d035d705e
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-06-09 17:24:19 +0300

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """app.py"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """method that calls storage.close"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ returns a JSON-formatted 404 status code"""
+    return jsonify({"error": "Not Found"}), 404
 
 
 if __name__ == "__main__":
