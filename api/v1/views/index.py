5174b5291443312062f8413e1ea7dc823d7028b7
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-06-09 16:45:18 +0300

    pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,3 @@
-from api.v1.views import app_views
-from flask import jsonify
-
-@app_views.get('/status')
-def get_status():
-    """gets status in json format"""
-    return jsonify({"status": "OK"})
-
 #!/usr/bin/python3
 """index"""
 
